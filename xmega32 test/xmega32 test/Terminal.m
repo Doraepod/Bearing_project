ce3c1750374c1489c8065b7153f24ac8062658b3
Grisha <sanjiii@rambler.ru>
2020-02-21 16:11:13 +0300

    string parse

--- xmega32 test/xmega32 test/Terminal.m
+++ xmega32 test/xmega32 test/Terminal.m
@@ -1,21 +1,25 @@
 %warning('warning', 'MATLAB:serial:fread:unsuccessfulRead');
-port = serial('COM7', 'BaudRate', 9600, 'DataBits', 8, 'StopBits', 1, 'Parity', 'None', 'timeout', 4);
+port = serial('COM1', 'BaudRate', 9600, 'DataBits', 8, 'StopBits', 1, 'Parity', 'None', 'timeout', 4);
 fopen(port);
 disp('open');
 i = 0;
 flag = 0;
 lastwarn('');
+buff = 11;
+strformat = 'a %d b %d c %d';
+a = [];
 while(~flag)
-    
-    a = fread(port, 1, 'int16');
-%     fprintf(port, 'i');
-    disp(a);
-    i = i + 1;
-%     pause(1);
-    if (strcmp(lastwarn,'Unsuccessful read: The specified amount of data was not returned within the Timeout period..'))
+    if (port.bytesAvailable >= (buff - 1))
+        a = fscanf(port, '%c', buff);
+        disp(a);
+    end
+    if (strcmp(lastwarn,'Unsuccessful read: A timeout occurred before the Terminator was reached or SIZE values were available..'))
         break
     end
+    if (~isempty(a))
+        sscanf(a, strformat)
+        a = [];
+    end
 end
-
 fclose(port);
 disp('closed');
