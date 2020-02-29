eefaeccdb4323a6ddadcf03db32f74ac4b6eddb2
Grisha <sanjiii@rambler.ru>
2020-02-29 18:49:16 +0300

    TWI

--- xmega32 test/xmega32 test/main_include_file.h
+++ xmega32 test/xmega32 test/main_include_file.h
@@ -1,3 +1,6 @@
+#ifndef DEFINE_CHECK
+#define DEFINE_CHECK
+
 #define F_CPU 32000000
 
 #include <avr/io.h>
@@ -7,4 +10,7 @@
 
 #include "GO_UART.h"
 #include "GO_ADC.h"
+#include "GO_TWI.h"
 #include "therightfrequency.h"
+
+#endif
