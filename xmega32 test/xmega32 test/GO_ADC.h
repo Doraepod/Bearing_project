6486a17f879d1e8b8c67a8cfbdce31cb43e87edd
lastincvizitor <last1nkvizitor@yandex.ru>
2020-03-02 18:03:20 +0300

    broken

--- xmega32 test/xmega32 test/GO_ADC.h
+++
@@ -1,2 +0,0 @@
-void adc_init(); // инициализация АЦП
-uint16_t adc_read(uint8_t port); // управление АЦП
--- xmega32 test/xmega32 test/GO_UART.h
+++
@@ -1,4 +0,0 @@
-void usart_init();
-void usart_send_char(char symbol);
-void usart_send_string(char *text);
-void usart_send_int(uint16_t data);
